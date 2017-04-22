4c6fbdb8e5f51b5710f6c89dcf2c4a8ff8370687
Esedli <esedli92e@mail.ru>
2017-04-22 11:50:25 +0400

    change

--- HelloWorld/src/com/esedli/git/HelloGit.java
+++ HelloWorld/src/com/esedli/git/HelloGit.java
@@ -7,9 +7,9 @@ public class HelloGit {
 		String names[] = {"A1","A2","A3","A4"};
 		
 		//print
-		for(int i=0; i<names.length; i++)
+		for(int i=0; i<names.length; i++){
 			System.out.println("Hello "+names[i]);
-
+		}
 	}
 
 }
