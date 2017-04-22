08d44def6f6b7fdb675881bd1944c734fdd31324
Esedli <esedli92e@mail.ru>
2017-04-22 11:37:53 +0400

    new lines

--- HelloWorld/src/com/esedli/git/HelloGit.java
+++ HelloWorld/src/com/esedli/git/HelloGit.java
@@ -3,8 +3,12 @@ package com.esedli.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		//a greeting
-		System.out.println("Hello World!");
+		
+		String names[] = {"A1","A2","A3","A4"};
+		
+		//print
+		for(int i=0; i<names.length; i++)
+			System.out.println("Hello "+names[i]);
 
 	}
 
